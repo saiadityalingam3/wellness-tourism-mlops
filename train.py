c07c1b344b9a333515ca9fffc41aca9e358e4624
saiadityalingam3 <saiadityalingam3@gmail.com>
2026-07-12 20:58:25 +0530

    Delete train.py

--- train.py
+++
@@ -1,6 +0,0 @@
-print("Starting Wellness Tourism MLOps Pipeline...")
-
-print("Data preprocessing completed.")
-print("Model training completed.")
-print("Model evaluation completed.")
-print("Pipeline executed successfully.")
