9333ee593ed9ee1e0dd7ffc15805dd00fb0417e8
saiadityalingam3 <saiadityalingam3@gmail.com>
2026-07-12 20:52:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,6 @@
+print("Starting Wellness Tourism MLOps Pipeline...")
 
+print("Data preprocessing completed.")
+print("Model training completed.")
+print("Model evaluation completed.")
+print("Pipeline executed successfully.")
